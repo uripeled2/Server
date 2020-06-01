6ea2f8927d9b21c1dd7f42772fab8bac63234bb2
Uri Peled <57635279+uripeled2@users.noreply.github.com>
2020-06-01 09:52:38 +0300

    Update mange.py

--- mange.py
+++ mange.py
@@ -5,21 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# app.config.from_pyfile('config.py')
-
-app.config["DEBUG"] = True
-app.config["SECRET_KEY"] = 'super-key'
-
-# MySQL setting
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="uripeled23",
-    password="2@WUxjwYxCvz9Km",
-    hostname="uripeled23.mysql.pythonanywhere-services.com",
-    databasename="uripeled23$comments",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
