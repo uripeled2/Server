b34e10802d23eecef486e27364e493bfcf7c4564
Uri Peled <57635279+uripeled2@users.noreply.github.com>
2020-06-01 09:53:21 +0300

    Update my_helper.py

--- my_helper.py
+++ my_helper.py
@@ -1,18 +1,5 @@
 from models import Activity, User
 
-# id = db.Column(db.Integer, primary_key=True)
-#     description = db.Column(db.String(400))
-#     spot = db.Column(db.String(100))
-#     policy_type = db.Column(db.Integer)  # 0 = no, 1 = modertet, 2 = strict
-#     penalty_price = db.Column(db.Integer)
-#     penalty_time = db.Column(db.Integer)
-#     days = db.Column(db.JSON)   # {"sunday": True, "monday": True, ...} or {"1": True, "2": True, ...}
-#     time_step = db.Column(db.JSON)  # {"start": hour, "end": hour} or {1: {"0": hour, "1": hour}, 2...}
-#     weekly = db.Column(db.Boolean)
-#
-#     # relationship handle
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
 
 def update_act(description, spot, policy_type, penalty_price, penalty_time, days, time_step, weekly, act_id):
     act = Activity.query.filter_by(id=act_id).first()
